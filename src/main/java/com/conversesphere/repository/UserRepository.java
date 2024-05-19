e059d44d873cc7fa802991dd6f764d9eec76b433
utharanambiar <utharanambiar@gmail.com>
2024-05-19 23:20:26 +0530

    Added security and model class

---
+++ src/main/java/com/conversesphere/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.conversesphere.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.conversesphere.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	public User findByEmail(String email);
+	
+	@Query("SELECT DISTINCT user FROM USER user WHERE user.fullName LIKE %:query% OR user.email LIKE %:query%")
+	public List<User> searchUser(@Param("query") String query);
+}
