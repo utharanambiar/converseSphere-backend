1045aa02262490be44639cf07af7943a639919f8
utharanambiar <utharanambiar@gmail.com>
2024-05-23 22:32:03 +0530

    user service added

---
+++ src/main/java/com/conversesphere/controller/TweetController.java
@@ -0,0 +1,18 @@
+package com.conversesphere.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.conversesphere.service.TweetService;
+import com.conversesphere.service.UserService;
+
+@RestController
+@RequestMapping("/api/tweet")
+public class TweetController {
+
+	private UserService userService;
+
+	private TweetService tweetService;
+
+}
---
+++ src/main/java/com/conversesphere/service/UserServiceImpl.java
@@ -0,0 +1,92 @@
+package com.conversesphere.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.conversesphere.config.JwtProvider;
+import com.conversesphere.exception.UserException;
+import com.conversesphere.model.User;
+import com.conversesphere.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserRepository userRepo;
+
+	@Autowired
+	private JwtProvider jwtProvider;
+
+	@Override
+	public User findUserById(Long userId) throws UserException {
+		// TODO Auto-generated method stub
+		User user = userRepo.findById(userId).orElseThrow(() -> new UserException("User not found with this ID"));
+		return user;
+	}
+
+	@Override
+	public User findUserProfileByJwt(String jwt) throws UserException {
+		// TODO Auto-generated method stub
+		String email = jwtProvider.getEmailFromToken(jwt);
+		User user = userRepo.findByEmail(email);
+
+		if (user == null)
+			throw new UserException("User not found with email" + email);
+		return user;
+	}
+
+	@Override
+	public User updateUser(Long userId, User user) throws UserException {
+		// TODO Auto-generated method stub
+		User userFound = findUserById(userId);
+
+		if (user.getFullName() != null) {
+			userFound.setFullName(user.getFullName());
+		}
+		if (user.getBio() != null) {
+			userFound.setBio(user.getBio());
+		}
+		if (user.getBannerImage() != null) {
+			userFound.setBannerImage(user.getBannerImage());
+		}
+		if (user.getProfileImage() != null) {
+			userFound.setProfileImage(user.getProfileImage());
+		}
+		if (user.getDOB() != null) {
+			userFound.setDOB(user.getDOB());
+		}
+		if (user.getLocation() != null) {
+			userFound.setLocation(user.getLocation());
+		}
+		if (user.getWebsite() != null) {
+			userFound.setWebsite(user.getWebsite());
+		}
+		return userRepo.save(userFound);
+	}
+
+	@Override
+	public User followUser(Long userId, User user) throws UserException {
+		// TODO Auto-generated method stub
+		User userToBeFollowed = findUserById(userId);
+		if (user.getFollowing().contains(userToBeFollowed) && userToBeFollowed.getFollowers().contains(user)) {
+			user.getFollowing().remove(userToBeFollowed);
+			userToBeFollowed.getFollowers().remove(user);
+		} else {
+			user.getFollowing().add(userToBeFollowed);
+			userToBeFollowed.getFollowers().add(user);
+		}
+		userRepo.save(userToBeFollowed);
+		userRepo.save(user);
+
+		return userToBeFollowed;
+	}
+
+	@Override
+	public List<User> searchUser(String query) {
+		// TODO Auto-generated method stub
+		return userRepo.searchUser(query);
+	}
+
+}
